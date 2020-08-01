cfd0fc17f9531f13fc424a0396afc905f689f1b8
DanielGuardado <59269194+DanielGuardado@users.noreply.github.com>
2020-08-01 03:46:50 -0400

    started working on dropdown

--- frontend/components/dropdown_menu/dropdown.jsx
+++ frontend/components/dropdown_menu/dropdown.jsx
@@ -1,1 +1,19 @@
-edfsdfsdf
+import React from 'react'
+
+function Dropdown(props) {
+  return (
+    <nav className="dropdown">
+      <ul className="dropdown-drop">{props.children}</ul>
+    </nav>
+  );
+}
+
+function DropdownItem(props){
+  return (
+    <li className="dropdown-items">
+      <a href="#" className="icon-button">
+        {props.icon}
+      </a>
+    </li>
+  )
+}
